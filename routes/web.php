e2d55cbbd5d48f909f324f508a6e74af42ec94b8
Moinul <moinulibr@gmail.com>
2026-04-28 23:56:49 +0600

    shifted test code in the test controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Test\RedisTestIngControllet;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,6 @@ Route::get('/', function () {
 
 Route::get('/redis-test', [RegisterController::class, 'redisTest']);
 
-Route::get('search-airlines',[RegisterController::class,'searchAirlines'])->name('search.airlines');
-Route::post('flight-book',[RegisterController::class, 'bookFlight'])->name('flight.book');
-Route::get('flight-booked-succes',[RegisterController::class, 'bookedFlightSuccess'])->name('flight.booked.succes');
+Route::get('search-airlines',[RedisTestIngControllet::class,'searchAirlines'])->name('search.airlines');
+Route::post('flight-book',[RedisTestIngControllet::class, 'bookFlight'])->name('flight.book');
+Route::get('flight-booked-succes',[RedisTestIngControllet::class, 'bookedFlightSuccess'])->name('flight.booked.succes');
